a72d76f53823a944521fccda3b1c54f22fb286ba
Volodymyr Shydlovsky <admin@itgarage.pro>
2020-08-16 11:50:51 +0000

    first commit

---
+++ start.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# check the bash shell script is being run by root
+if [ "$EUID" -ne 0 ]
+then 
+    echo 'this script must be run with sudo'
+    exit
+fi
+
+# link functions
+ln -s functions.sh $PWD/p5/functions.sh
+
+# view files in p5
+find $PWD/p5/ -xdev -type f -name '5.*.sh' | xargs stat | grep 'File'
+
+# set files on p5 executable
+find $PWD/p5/ -xdev -type f -name '5.*.sh' | xargs chmod +x
+
+# run all executable sctipts in /p5
+run-parts --regex '\.sh$'  $PWD/p5
