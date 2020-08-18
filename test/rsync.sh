3ee76545eecb946944b370a31f315eed1b0b944f
Volodymyr Shydlovsky <admin@itgarage.pro>
2020-08-18 22:15:36 +0300

    comment

--- test/rsync.sh
+++ test/rsync.sh
@@ -7,4 +7,4 @@ rsync --recursive --links /home/ef/cis-script usrv:/home/ef/cis-backup/"$(date +
 # minute=$(date +"%M")
 # rsync --rsync-path="mkdir -p /home/ef/cis-backup/"$day"/"$hour"/"$minute"/" && rsync --recursive --progress /home/ef/cis-script usrv:/home/ef/cis-backup/"$day"/"$hour"/"$minute"/
 
-rsync --rsync-path="mkdir -p usrv:/home/ef/cis-backup/"$(date +"%Y-%m-%d")"/"$(date +"%H")"/"$(date +"%M")"/" && rsync --recursive /home/ef/cis-script usrv:/home/ef/cis-backup/"$(date +"%Y-%m-%d")"/"$(date +"%H")"/"$(date +"%M")"/
+# rsync --rsync-path="mkdir -p usrv:/home/ef/cis-backup/"$(date +"%Y-%m-%d")"/"$(date +"%H")"/"$(date +"%M")"/" && rsync --recursive /home/ef/cis-script usrv:/home/ef/cis-backup/"$(date +"%Y-%m-%d")"/"$(date +"%H")"/"$(date +"%M")"/
